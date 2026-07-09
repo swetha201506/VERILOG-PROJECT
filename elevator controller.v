4085fe14eee3bef85a3061d12d8e1d55ff9b53eb
swetha201506 <swethasri2360@gmail.com>
2026-07-09 20:49:52 +0530

    Add elevator controller module with floor logic
    Implement elevator controller module with floor control logic.

--- elevator controller.v
+++ elevator controller.v
@@ -1,1 +1,24 @@
+module elevator_controller(
+    input clk,
+    input rst,
+    input up,
+    input down,
+    output reg [1:0] floor
+);
 
+always @(posedge clk or posedge rst)
+begin
+    if(rst)
+        floor <= 2'd0;
+
+    else
+    begin
+        if(up && floor < 2)
+            floor <= floor + 1;
+
+        else if(down && floor > 0)
+            floor <= floor - 1;
+    end
+end
+
+endmodule
