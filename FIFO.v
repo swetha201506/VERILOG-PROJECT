60ce0479f619c1a2b6b7beb055824068c9070aae
swetha201506 <swethasri2360@gmail.com>
2026-07-09 20:38:03 +0530

    Implement FIFO module with read and write functionality

--- FIFO.v
+++ FIFO.v
@@ -1,1 +1,38 @@
+module fifo(
+    input clk,
+    input rst,
+    input wr_en,
+    input rd_en,
+    input [7:0] data_in,
+    output reg [7:0] data_out
+);
 
+reg [7:0] mem [3:0];
+
+reg [1:0] wr_ptr;
+reg [1:0] rd_ptr;
+
+always @(posedge clk or posedge rst)
+begin
+    if(rst)
+    begin
+        wr_ptr <= 0;
+        rd_ptr <= 0;
+    end
+    else
+    begin
+        if(wr_en)
+        begin
+            mem[wr_ptr] <= data_in;
+            wr_ptr <= wr_ptr + 1;
+        end
+
+        if(rd_en)
+        begin
+            data_out <= mem[rd_ptr];
+            rd_ptr <= rd_ptr + 1;
+        end
+    end
+end
+
+endmodule
