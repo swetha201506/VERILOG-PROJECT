73ed82cbbffcd23b0107d407073b2e0ec07d01ba
swetha201506 <swethasri2360@gmail.com>
2026-07-09 20:28:42 +0530

    Add tb_traffic_light.v

--- traffic_light.v
+++ traffic_light.v
@@ -1,1 +1,24 @@
-traffic_light.verilog code
+module tb_traffic_light;
+
+reg clk;
+reg rst;
+wire [2:0] light;
+
+traffic_light uut(
+    .clk(clk),
+    .rst(rst),
+    .light(light)
+);
+
+always #5 clk = ~clk;
+
+initial begin
+    clk = 0;
+    rst = 1;
+
+    #10 rst = 0;
+
+    #50 $finish;
+end
+
+endmoduletraffic_light.verilog code
