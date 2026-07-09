5139ac55658d96b9c06dadf7b5959de0130a061a
swetha201506 <swethasri2360@gmail.com>
2026-07-09 20:52:34 +0530

    Implement SPI master module with data shifting

--- spi_master.v
+++ spi_master.v
@@ -1,1 +1,26 @@
+module spi_master(
+    input clk,
+    input rst,
+    input [7:0] data_in,
+    output reg mosi,
+    output reg sclk
+);
 
+reg [2:0] count;
+
+always @(posedge clk or posedge rst)
+begin
+    if(rst)
+    begin
+        count <= 0;
+        sclk <= 0;
+    end
+    else
+    begin
+        mosi <= data_in[7-count];
+        count <= count + 1;
+        sclk <= ~sclk;
+    end
+end
+
+endmodule
