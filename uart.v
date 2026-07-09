d6e1482833dd625abbfc90dc827999a9ec68d3c5
swetha201506 <swethasri2360@gmail.com>
2026-07-09 20:47:09 +0530

    Add UART transmitter module with basic functionality

--- uart.v
+++ uart.v
@@ -1,1 +1,17 @@
+module uart_tx(
+    input clk,
+    input rst,
+    input tx_start,
+    input [7:0] data_in,
+    output reg tx
+);
 
+always @(posedge clk or posedge rst)
+begin
+    if(rst)
+        tx <= 1'b1;
+    else if(tx_start)
+        tx <= data_in[0];
+end
+
+endmodule
