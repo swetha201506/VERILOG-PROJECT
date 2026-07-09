ee928b6cee9ced80443be9ea52db37eb73172792
swetha201506 <swethasri2360@gmail.com>
2026-07-09 20:55:53 +0530

    Create testbench for 8-bit ALU functionality
    Added a testbench for the 8-bit ALU with various operations.

--- tb_8_bit_alu.v
+++ tb_8_bit_alu.v
@@ -1,1 +1,37 @@
+module tb_alu;
 
+reg [7:0] a,b;
+reg [2:0] sel;
+wire [7:0] result;
+
+alu_8bit uut(
+    .a(a),
+    .b(b),
+    .sel(sel),
+    .result(result)
+);
+
+initial begin
+
+    a = 8'd10;
+    b = 8'd5;
+
+    sel = 3'b000; #10;
+    $display("ADD Result = %d", result);
+
+    sel = 3'b001; #10;
+    $display("SUB Result = %d", result);
+
+    sel = 3'b010; #10;
+    $display("AND Result = %d", result);
+
+    sel = 3'b011; #10;
+    $display("OR Result = %d", result);
+
+    sel = 3'b100; #10;
+    $display("XOR Result = %d", result);
+
+    $finish;
+end
+
+endmodule
