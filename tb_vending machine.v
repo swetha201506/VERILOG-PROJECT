b45aecb5794b17b8e1a4043acb5deab10c00e95c
swetha201506 <swethasri2360@gmail.com>
2026-07-09 20:43:43 +0530

    Add testbench for vending machine module

--- tb_vending machine.v
+++ tb_vending machine.v
@@ -1,1 +1,58 @@
+module tb_vending_machine;
 
+reg clk;
+reg rst;
+reg [1:0] coin;
+
+wire dispense;
+
+vending_machine uut(
+    .clk(clk),
+    .rst(rst),
+    .coin(coin),
+    .dispense(dispense)
+);
+
+// Clock Generation
+always #5 clk = ~clk;
+
+initial begin
+
+    clk = 0;
+    rst = 1;
+    coin = 2'b00;
+
+    // Reset
+    #10 rst = 0;
+
+    // Insert Rs.5
+    coin = 2'b01;
+    #10;
+    $display("Coin = 5, Dispense = %b", dispense);
+
+    // Insert another Rs.5
+    coin = 2'b01;
+    #10;
+    $display("Coin = 5, Dispense = %b", dispense);
+
+    // Wait for dispense
+    coin = 2'b00;
+    #10;
+    $display("Chocolate Dispensed = %b", dispense);
+
+    // Insert Rs.10 directly
+    coin = 2'b10;
+    #10;
+    $display("Coin = 10, Dispense = %b", dispense);
+
+    // Wait for dispense
+    coin = 2'b00;
+    #10;
+    $display("Chocolate Dispensed = %b", dispense);
+
+    #20;
+    $finish;
+
+end
+
+endmodule
