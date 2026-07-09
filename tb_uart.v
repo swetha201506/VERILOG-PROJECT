bec495f8003ec5b596f68a516a6f7d39d4118259
swetha201506 <swethasri2360@gmail.com>
2026-07-09 20:48:32 +0530

    Create tb_uart_tx module for UART testing
    Added testbench for UART transmission with clock and reset.

--- tb_uart.v
+++ tb_uart.v
@@ -1,1 +1,46 @@
+module tb_uart_tx;
 
+reg clk;
+reg rst;
+reg tx_start;
+reg [7:0] data_in;
+
+wire tx;
+
+uart_tx uut(
+    .clk(clk),
+    .rst(rst),
+    .tx_start(tx_start),
+    .data_in(data_in),
+    .tx(tx)
+);
+
+// Clock Generation
+always #5 clk = ~clk;
+
+initial begin
+
+    clk = 0;
+    rst = 1;
+    tx_start = 0;
+    data_in = 8'b10101010;
+
+    // Reset
+    #10 rst = 0;
+
+    // Start Transmission
+    tx_start = 1;
+    #10;
+
+    tx_start = 0;
+
+    // Display Output
+    #10;
+    $display("TX = %b", tx);
+
+    #50;
+    $finish;
+
+end
+
+endmodule
